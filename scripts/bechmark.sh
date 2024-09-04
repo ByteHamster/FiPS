9eeafd50793f7ba868decbef7713fca01321ea5a
ByteHamster <info@bytehamster.com>
2024-09-04 09:52:32 +0200

    Support gamma in benchmark

--- scripts/bechmark.sh
+++ scripts/bechmark.sh
@@ -4,6 +4,8 @@ strings Benchmark | grep " -m"
 
 for lineSize in 1024 512 256 128 64; do
   for offsetSize in 32 16; do
-    ./Benchmark --numObjects 100M --numQueries 100M --lineSize $lineSize --offsetSize $offsetSize
+    for gamma in $(seq 1.0 0.1 3.0); do
+      ./Benchmark --numObjects 100M --numQueries 100M --lineSize $lineSize --offsetSize $offsetSize --gamma $gamma
+    done
   done
 done
